0954dde7104b54ed0969efcc59acd2e6c6805cc3
Lukas Kollmer <hey@lukaskollmer.de>
2026-04-10 16:57:12 +0200

    improve swift-syntax compatibility

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .library(name: "ThreadLocal", targets: ["ThreadLocal"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0")
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "601.0.0"..<"604.0.0")
     ],
     targets: [
         .macro(
